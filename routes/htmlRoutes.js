7e08de97a7140d035366bcbaac0439c1ed9102b9
Quaker10 <steveszumski@Steves-MacBook-Pro.local>
2018-09-08 13:12:45 -0700

    configured react

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,16 +1,23 @@
 var db = require("../models");
+var path = require('path');
 
 module.exports = function(app) {
   // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
-  });
+  // app.get("/", function(req, res) {
+  //   db.Example.findAll({}).then(function(dbExamples) {
+  //     res.render("index", {
+  //       msg: "Welcome!",
+  //       examples: dbExamples
+  //     });
+  //   });
+  // });
 
+
+  // React
+
+  app.get("/", function(req, res) {
+    res.sendFile( path.join(__dirname, '../', 'public', 'build', 'index.html'))
+  })
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
     db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
