f59b4fced7529739395b65c0d91597cbbb61ba3f
imalam64 <imalam64@gmail.com>
2018-09-12 21:46:24 -0700

    styled calendar

--- public/src/App.js
+++ public/src/App.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import Calendar from './Components/Calendar/Calendar';
-import CalendarCard from './Components/CalendarCard/CalendarCard';
-import Forms from './Components/Forms/Forms';
+//import CalendarCard from './Components/CalendarCard/CalendarCard';
+//import Forms from './Components/Forms/Forms';
 
 class App extends Component {
   render() {
@@ -11,8 +11,8 @@ class App extends Component {
       <div className="App">
        <Navbar />
        <Calendar />
-       <CalendarCard />
-       <Forms />
+{/*  <CalendarCard /> */}
+{/*<Forms />*/}
       </div>
     );
   }
