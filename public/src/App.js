45dfed65306aba7636e3e7a63ae13ba60cf83462
imalam64 <imalam64@gmail.com>
2018-09-12 21:20:03 -0700

    back to the start

--- public/src/App.js
+++ public/src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import Calendar from './Components/Calendar/Calendar';
+import CalendarCard from './Components/CalendarCard/CalendarCard';
+import Forms from './Components/Forms/Forms';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+       <Navbar />
+       <Calendar />
+       <CalendarCard />
+       <Forms />
       </div>
     );
   }
