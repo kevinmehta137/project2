6e3c8c2b0900507afb24e2176baad37c296f4aa2
Quaker10 <steveszumski@Steves-MacBook-Pro.local>
2018-09-12 20:11:22 -0700

    fixed react

--- public/src/App.js
+++ public/src/App.js
@@ -1,28 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import Home from "./pages/home";
-import Calendar from "./pages/Calendar/components/Calendar";
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-        <header>
-          <div id="logo">
-            <span className="icon">date_range</span>
-            <span>
-              react<b>calendar</b>
-            </span>
-          </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
         </header>
-        <main>
-          <Calendar />
-        </main>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
       </div>
-      </Router>
     );
   }
 }
