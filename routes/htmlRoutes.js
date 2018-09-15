af28cba1100bb665a18bf39b39760b95c6e87f85
KEVIN MEHTA <kevinmehta137@gmail.com>
2018-09-15 10:19:24 -0700

    Create and get api route

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,34 +1,39 @@
-// var db = require("../models");
-// var path = require('path');
+var db = require("../models");
+var path = require('path');
 
-// module.exports = function(app) {
-//   // Load index page
-//   // app.get("/", function(req, res) {
-//   //   db.Example.findAll({}).then(function(dbExamples) {
-//   //     res.render("index", {
-//   //       msg: "Welcome!",
-//   //       examples: dbExamples
-//   //     });
-//   //   });
-//   // });
+module.exports = function(app) {
+  // Load index page
+  app.get("/api/examples", function(req, res) {
+    db.Example.findAll({
+      attributes:['employer_id','employer_name','employer_email','employer_password','employer_confirmpassword'],
+      WHERE:{
+        id:req.params.id,
+        email:req.params.employer_email,
+        password:req.params.employer_password
+      }
+    }).then(function(dbExamples) {
+      res.json(dbExamples);
+    });
+  });
 
 
-//   // React
 
-//   app.get("/", function(req, res) {
-//     res.sendFile( path.join(__dirname, '../', 'public', 'build', 'index.html'))
-//   })
-//   // Load example page and pass in an example by id
-//   app.get("/example/:id", function(req, res) {
-//     db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-//       res.render("example", {
-//         example: dbExample
-//       });
-//     });
-//   });
+  // React
 
-//   // Render 404 page for any unmatched routes
-//   app.get("*", function(req, res) {
-//     res.sendFile( path.join(__dirname, '../', 'public', 'build', 'index.html'))
-//   });
-// };
+  app.get("/", function(req, res) {
+    res.sendFile( path.join(__dirname, '../', 'public', 'build', 'index.html'))
+  })
+  // Load example page and pass in an example by id
+  app.get("/example/:id", function(req, res) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+      res.render("example", {
+        example: dbExample
+      });
+    });
+  });
+
+  // Render 404 page for any unmatched routes
+  app.get("*", function(req, res) {
+    res.sendFile( path.join(__dirname, '../', 'public', 'build', 'index.html'))
+  });
+};
